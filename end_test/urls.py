6882ef97d21c8ec9c36628017030d50295620a4f
bernhard <bernhard@isys.uni-klu.ac.at>
2019-06-27 17:29:59 +0200

    added login screen

--- end_test/urls.py
+++ end_test/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('start_test', views.start_test, name='start_test'),
     path('check_test', views.check_test, name='check_test'),
 ]
--- end_test/views.py
+++ end_test/views.py
@@ -7,101 +7,126 @@ _NUMBER_OF_ANSWERS = 3
 _NUMBER_OF_QUESTIONS = 10
 _CORRECT_ANSWERS = ['q1a2','q2a2','q3a2','q4a1','q5a1','q6a3','q7a3','q8a2','q9a2','q10a1']
 _QUESTIONS = {
-    	'1.	Was ist <b> keine </b> Cannabinoid-Art?': ['Endocannabinoide', 'Plasmacannabinoide', 'Phytocannabinoide'],
-    	'2.	THC und CBD: Welche Aussage stimmt <b>nicht</b>?': ['THC hat viele gesundheitlich vorteilhafte Wirkungen.', 'CBD und THC sind beide frei verkäuflich.', 'CBD hat keine Rauschwirkung.'],
-    	'3.	Unter welcher Produktbezeichnung darf CBD <b>nicht</b> verkauft werden? ': ['Aromaprodukt ', 'Nahrungsmittelzusatz', 'Tabakersatz'],
-    	'4.	Bei welchen Beschwerden kann CBD helfen? ': ['Parkinson', 'Lähmungserscheinungen ', 'Leukämie'],
-    	'5.	Was bedeutet AC-zertifiziert? ': ['Gütesiegel für CBD-Produkte', 'Staatliches Siegel für CBD-Produkte', 'Gütesiegel für Regionalität'],
-    	'6.	Wie viel Prozent CBD sind in Kristallen enthalten? ': ['78,9%', '89,9%', '98,9%'],
-    	'7.	Was bedeutet CBD Vollextrakt ': ['Im Produkt ist nur der Wirkstoff CBD enthalten.', 'Das Produkt ist in Kristallform mit sehr hohem CBD-Anteil.', 'Im Produkt sind alle Wirkstoffe der Pflanze enthalten.'],
-    	'8.	Welches CBD-Öl Pur bieten wir nicht an? ': ['5%', '15%', '25%'],
-    	'9.	Welche Geschmacksrichtung bieten wir <b>nicht</b> an?': ['Ananas', 'Mango', 'Zitrone'],
-    	'10. Welche weiteren Produkte bieten wir an bzw. sind in Planung? ': ['Veterinärmedizin', 'Kleidung ', 'Rohstoffmaterial '],
+        '1. Was ist <b> keine </b> Cannabinoid-Art?': ['Endocannabinoide', 'Plasmacannabinoide', 'Phytocannabinoide'],
+        '2. THC und CBD: Welche Aussage stimmt <b>nicht</b>?': ['THC hat viele gesundheitlich vorteilhafte Wirkungen.', 'CBD und THC sind beide frei verkäuflich.', 'CBD hat keine Rauschwirkung.'],
+        '3. Unter welcher Produktbezeichnung darf CBD <b>nicht</b> verkauft werden? ': ['Aromaprodukt ', 'Nahrungsmittelzusatz', 'Tabakersatz'],
+        '4. Bei welchen Beschwerden kann CBD helfen? ': ['Parkinson', 'Lähmungserscheinungen ', 'Leukämie'],
+        '5. Was bedeutet AC-zertifiziert? ': ['Gütesiegel für CBD-Produkte', 'Staatliches Siegel für CBD-Produkte', 'Gütesiegel für Regionalität'],
+        '6. Wie viel Prozent CBD sind in Kristallen enthalten? ': ['78,9%', '89,9%', '98,9%'],
+        '7. Was bedeutet CBD Vollextrakt ': ['Im Produkt ist nur der Wirkstoff CBD enthalten.', 'Das Produkt ist in Kristallform mit sehr hohem CBD-Anteil.', 'Im Produkt sind alle Wirkstoffe der Pflanze enthalten.'],
+        '8. Welches CBD-Öl Pur bieten wir nicht an? ': ['5%', '15%', '25%'],
+        '9. Welche Geschmacksrichtung bieten wir <b>nicht</b> an?': ['Ananas', 'Mango', 'Zitrone'],
+        '10. Welche weiteren Produkte bieten wir an bzw. sind in Planung? ': ['Veterinärmedizin', 'Kleidung ', 'Rohstoffmaterial '],
     }
 
 
 def index(request):
-    template = loader.get_template('end_test/index.html')
-    context = {
-        'questions': _QUESTIONS
-    }
+         
+    template = loader.get_template('end_test/login.html')
+    context = {}
+    request.session['start'] = True
+    return HttpResponse(template.render(context, request))    
 
-    return HttpResponse(template.render(context, request))
+def start_test(request):
+    if request.method == 'POST': 
+        request.session['start'] = False
+        form = request.POST
+        firstname = form['firstname']
+        lastname = form['lastname']
+
+        request.session['firstname'] = firstname
+        request.session['lastname'] = lastname
+
+
+        template = loader.get_template('end_test/index.html')
+        context = {
+            'questions': _QUESTIONS
+        }
+
+        return HttpResponse(template.render(context, request))
+    else:
+        return index(request)
+
 
 
 def check_test(request):
-	if request.method =='POST':
-		form = request.POST
-		firstname = form['firstname']
-		lastname = form['lastname']
-		test_date = timezone.now()
-		answers = []
-		questions = _QUESTIONS
+    if request.method =='POST':
+        form = request.POST
+        firstname = request.session['firstname']
+        lastname = request.session['lastname']
+        test_date = timezone.now()
+        answers = []
+        questions = _QUESTIONS
 
-		correct_or_incorrect = []
-		correct_answers = 0
-		for i in range(_NUMBER_OF_QUESTIONS):
-			if 'question'+str(i+1) in form:
-				given_answer = form['question'+str(i+1)]
-				answers.append(given_answer)
-				if(given_answer in _CORRECT_ANSWERS):
-					correct_answers += 1
-					correct_or_incorrect.append(correct_answer_colors(i))
-				else:
-					correct_or_incorrect.append(false_answer_colors(i, given_answer))
-			else:
-				correct_or_incorrect.append(false_answer_colors(i, '0'))
-
-
-				
-		print(firstname)
-		print(lastname)
-		print(test_date)
-		print('ANSWERS:', answers)
-		print(correct_answers/len(_CORRECT_ANSWERS))
-
-		answers_to_store = TestParticipant(firstname=firstname, lastname=lastname, 
-			test_date=test_date, answers=answers, result=correct_answers/len(_CORRECT_ANSWERS))
+        correct_or_incorrect = []
+        correct_answers = 0
+        for i in range(_NUMBER_OF_QUESTIONS):
+            if 'question'+str(i+1) in form:
+                given_answer = form['question'+str(i+1)]
+                answers.append(given_answer)
+                if(given_answer in _CORRECT_ANSWERS):
+                    correct_answers += 1
+                    correct_or_incorrect.append(correct_answer_colors(i))
+                else:
+                    correct_or_incorrect.append(false_answer_colors(i, given_answer))
+            else:
+                correct_or_incorrect.append(false_answer_colors(i, '0'))
 
 
-		#answers_to_store.save()
+                
+        print(firstname)
+        print(lastname)
+        print(test_date)
+        print('ANSWERS:', answers)
+        print(correct_answers/len(_CORRECT_ANSWERS))
 
-		template = loader.get_template('end_test/results.html')
-		context = {
-			'questions': _QUESTIONS,
-			'results': correct_or_incorrect,
-			'answers': _CORRECT_ANSWERS,
-		}
+        answers_to_store = TestParticipant(firstname=firstname, lastname=lastname, 
+            test_date=test_date, answers=answers, result=correct_answers/len(_CORRECT_ANSWERS))
 
-	return HttpResponse(template.render(context, request))
+
+        #answers_to_store.save()
+
+        template = loader.get_template('end_test/results.html')
+        context = {
+            'questions': _QUESTIONS,
+            'results': correct_or_incorrect,
+            'answers': _CORRECT_ANSWERS,
+            'number_correct_answers':correct_answers,
+        }
+
+        #delte session to in order to prevent the user to retry the test_date
+        del request.session['firstname']    
+        del request.session['lastname']
+
+    return HttpResponse(template.render(context, request))
 
 def correct_answer_colors(question_id):
-	correct_answer = _CORRECT_ANSWERS[question_id]
-	correct_postion = int(correct_answer[-1:])
-	
-	background_colors = []
-	for i in range(1,_NUMBER_OF_ANSWERS+1):
-		if(i == correct_postion):
-			background_colors.append('green')
-		else:
-			background_colors.append('none')
+    correct_answer = _CORRECT_ANSWERS[question_id]
+    correct_postion = int(correct_answer[-1:])
+    
+    background_colors = []
+    for i in range(1,_NUMBER_OF_ANSWERS+1):
+        if(i == correct_postion):
+            background_colors.append('green')
+        else:
+            background_colors.append('none')
 
-	return background_colors
+    return background_colors
 
 def false_answer_colors(question_id, given_answer):
-	given_answer_position = int(given_answer[-1:])
+    given_answer_position = int(given_answer[-1:])
 
-	correct_answer = _CORRECT_ANSWERS[question_id]
-	correct_postion = int(correct_answer[-1:])
-	
-	background_colors = []
-	for i in range(1,_NUMBER_OF_ANSWERS+1):
-		if(i == correct_postion):
-			background_colors.append('green')
-		elif(i == given_answer_position):
-			background_colors.append('red')
-		else:
-			background_colors.append('none')
+    correct_answer = _CORRECT_ANSWERS[question_id]
+    correct_postion = int(correct_answer[-1:])
+    
+    background_colors = []
+    for i in range(1,_NUMBER_OF_ANSWERS+1):
+        if(i == correct_postion):
+            background_colors.append('green')
+        elif(i == given_answer_position):
+            background_colors.append('red')
+        else:
+            background_colors.append('none')
 
-	return background_colors
+    return background_colors
 
